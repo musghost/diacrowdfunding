b18a724734d036bbe77d30e03a45f9ebb1690563
YareArista <yarearista@gmail.com>
2017-07-04 18:33:06 -0500

    Add extra fields to the form

--- src/app/contact/contact.js
+++ src/app/contact/contact.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import {Header} from './../header';
 import {Footer} from './../footer';
+import axios from 'axios';
 
 const styles = {
   image: {
-    backgroundImage: 'url(assets/contact/contact.jpg)'
+    backgroundImage: 'url(assets/contact/contact.jpg)',
+    backgroundSize: '110% 90%',
+    backgroundPosition: 'center'
   }
 };
 
@@ -13,15 +16,82 @@ export class Contact extends Component {
   constructor() {
     super();
     this.state = {
-      submited: true
+      submited: true,
+      name: '',
+      email: '',
+      phone: '',
+      topic: '',
+      message: '',
+      occupation: '',
+      business: '',
+      lastName: '',
+      identification: ''
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
   }
 
-  handleSubmit(e) {
+  handleOnSubmit(e) {
     e.preventDefault();
     this.setState({
       submited: false
+    });
+  }
+
+  handleSetName = event => {
+    this.setState({name: event.target.value});
+  }
+  handleSetEmail = event => {
+    this.setState({email: event.target.value});
+  }
+  handleSetNumber = event => {
+    this.setState({number: event.target.value});
+  }
+  handleSetTopic = event => {
+    this.setState({topic: event.target.value});
+  }
+  handleSetMessage = event => {
+    this.setState({message: event.target.value});
+  }
+  handleSetOccupation = event => {
+    this.setState({occupation: event.target.value});
+  }
+  handleSetBusiness = event => {
+    this.setState({business: event.target.value});
+  }
+  handleSetLastName = event => {
+    this.setState({lastName: event.target.value});
+  }
+  handleSetIdentification = event => {
+    this.setState({identification: event.target.value});
+  }
+
+  handleSubmit = () => {
+    console.log('entro');
+    console.log(this.state.name);
+    console.log(this.state.email);
+    console.log(this.state.number);
+    console.log(this.state.topic);
+    console.log(this.state.message);
+    console.log(this.state.occupation);
+    console.log(this.state.business);
+    console.log(this.state.lastName);
+    console.log(this.state.identification);
+    axios.post('http://dia-api.mellow.online/api/Contacts', {
+      name: this.state.name,
+      email: this.state.email,
+      number: this.state.number,
+      topic: this.state.topic,
+      message: this.state.message,
+      occupation: this.state.occupation,
+      business: this.state.business,
+      lastName: this.state.lastName,
+      identification: this.state.identification
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
     });
   }
 
@@ -35,31 +105,53 @@ export class Contact extends Component {
             <div className="row">
               <div className="col-sm-6 instructions">
                 <div>
-                  <form onSubmit={this.handleSubmit}>
+                  <form onSubmit={this.handleOnSubmit}>
                     {this.state.submited ? (<div>
                       <div className="row">
                         <div className="col-sm-6">
-                          <input type="text" placeholder="Nombre *"/>
+                          <input type="text" placeholder="Nombre *" required onChange={this.handleSetName}/>
                         </div>
                         <div className="col-sm-6">
-                          <input type="email" placeholder="E-Mail *"/>
+                          <input type="text" placeholder="Apellidos *" required onChange={this.handleSetLastName}/>
                         </div>
                       </div>
                       <div className="row">
                         <div className="col-sm-6">
-                          <input type="text" placeholder="Teléfonos"/>
+                          <select required onChange={this.handleSetOccupation}>
+                            <option value="ocupacion">Ocupación *</option>
+                            <option value="Estudiante">Estudiante</option>
+                            <option value="Empresario">Empresario</option>
+                            <option value="Emprendedor">Emprendedor</option>
+                          </select>
                         </div>
                         <div className="col-sm-6">
-                          <input type="text" placeholder="Tópico"/>
+                          <input type="text" placeholder="CURP o RFC *" required onChange={this.handleSetIdentification} className="uppercase"/>
+                          <small className="small">Consulta tu <a href="https://consultas.curp.gob.mx/CurpSP/inicio2_2.jsp" target="_blank">CURP</a> o <a href="http://www.mi-rfc.com.mx/consulta-rfc-homoclave" target="_blank">RFC</a></small>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-sm-6">
+                          <input type="text" placeholder="Empresa o Proyecto *" required onChange={this.handleSetBusiness}/>
+                        </div>
+                        <div className="col-sm-6">
+                          <input type="email" placeholder="E-Mail *" required onChange={this.handleSetEmail}/>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-sm-6">
+                          <input type="number" placeholder="Teléfonos" required onChange={this.handleSetNumber}/>
+                        </div>
+                        <div className="col-sm-6">
+                          <input type="text" placeholder="Tópico" required onChange={this.handleSetTopic}/>
                         </div>
                       </div>
                       <div className="row">
                         <div className="col-sm-12">
-                          <textarea placeholder="Mensaje *" rows="10"></textarea>
+                          <textarea placeholder="Mensaje *" rows="10" required onChange={this.handleSetMessage}></textarea>
                         </div>
                       </div>
                       <div className="text-center">
-                        <button type="submit">Enviar Correo</button>
+                        <button type="submit" onClick={this.handleSubmit}>Enviar Correo</button>
                       </div>
                     </div>) : (<p className="super-big-text">¡Gracias por enviar tus datos!</p>)}
                   </form>
--- src/app/footer.js
+++ src/app/footer.js
@@ -26,7 +26,7 @@ export class Footer extends Component {
             <div className="col-md-11">
               <div className="text-right">
                 <h3>¡FORMA PARTE DE LA COMUNIDAD DE<br/>FONDEO COLECTIVO EN NUESTRO PAÍS!</h3>
-                <h4>ESTE 5 DE OCT. SE CELEBRARÁ EL “DÍA CROWDFUNDING”</h4>
+                <h4>ESTE 13 DE SEP. SE CELEBRARÁ EL “DÍA CROWDFUNDING”</h4>
                 <p>Evento en línea que invita a inversionistas y a<br/>
                   emprendedores a formar parte de la comunidad de<br/>
                 fondeo colectivo más grande de México.</p>
